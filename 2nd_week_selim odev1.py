17be7076282a5f4685116e2b0fd4e4dd63350e64
kromdeniz <62304357+kromdeniz@users.noreply.github.com>
2020-03-25 01:17:44 +0100

    Add files via upload

---
+++ 2nd_week_selim odev1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 17:04:36 2020
+
+@author: mnm
+"""
+
+
+# num = int(input("Enter a number..."))
+# if num > 10:
+#   print("Selim akilli")
+# else:
+# 	print("Selim salak")
+
+
+
+          
+    

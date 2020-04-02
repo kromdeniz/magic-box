a93ccb196b7b40c6e4825263913df8dba3ff6a09
kromdeniz <62304357+kromdeniz@users.noreply.github.com>
2020-04-03 00:04:21 +0200

    update 2nd_week_selim_odev2.py
    update 2nd_week_selim_odev2.py

--- 2nd_week_selim odev2.py
+++ 2nd_week_selim odev2.py
@@ -1,24 +1,16 @@
-# for x in range(10):
-#     print("turkiye turklerindir")
+for x in range(10):
+    print("turkiye turklerindir")
 
+e = 0
+while e < 10:
+    print("turkiye turklerindir")
+    e += 1
+    
 
-# euro = float(input("euro bugun kac TL? "))
-# while euro > 10:
-#     print("yaktin bizi tayyip")
-# else:print("turkiye turklerindir")
-
-
-
-# euro = float(input("euro bugun kac TL "))
-# while euro > 10:   
-#         print(input("yaktin bizi tayyip peki simdi kac TL? "))
-##         burda kucuk bir sorun
-##         oop un icinde input<10 olsa bile output degismiyor    
-# for x in range(10):
-#     print("turkiye turklerindir")
-
-# euro = 1
-# while euro < 11:
-#     print("turkiye turklerindir")
-#     euro = euro + 1
-# else:print("yaktin bizi tayyip")
+e = int(input("euro kac tl?.."))
+while e > 10:
+    e = int(input("yaktin bizi tayyip, peki simdi kac tl?.."))
+    print(e)
+    if e < 10:
+        break
+print("turkiye turklerindir")
